5f9b3cee23ed028249c8c2908d9a693979319169
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-05-22 12:50:35 -0400

    Research

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"dish_dash_go/extract"
+	"dish_dash_go/transform"
+	"sync"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var waitgroup sync.WaitGroup
+	urls := []string{
+		"https://icecreamfromscratch.com/cornbread-ice-cream/",
+		"https://icecreamfromscratch.com/lime-ice-cream/",
+		"https://icecreamfromscratch.com/honey-lavender-ice-cream/",
+		"https://icecreamfromscratch.com/blueberry-cheesecake-ice-cream/",
+	}
+
+	waitgroup.Add(len(urls))
+	for _, url := range urls {
+		go func(u string) {
+			defer waitgroup.Done()
+			html := extract.PageHTML(u)
+			transform.ParseHTML(html)
+		}(url)
+	}
+	waitgroup.Wait()
 }
