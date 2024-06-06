db586b55521d11c18944b0144889486f89655266
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-06-05 20:46:54 -0400

    refining error handling

--- extract/extract.go
+++ extract/extract.go
@@ -1,19 +1,37 @@
 package extract
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/asaskevich/govalidator"
 )
 
-func PageHTML(url string) string {
-	resp, err := http.Get(string(url))
-	if err != nil {
-		log.Fatalln(err)
+func PageHTML(urlString string) (string, error) {
+	// Validate URL
+	u := govalidator.IsURL(urlString)
+	if !u {
+		errorMessage := fmt.Errorf("error - Invalid URL: %s", urlString)
+		return "", errorMessage
 	}
+	// Retrieve HTML data
+	resp, err := http.Get(string(urlString))
+	fmt.Println(err)
+	if err != nil {
+		errorMessage := fmt.Errorf("error - %v", err)
+		return "", errorMessage
+	}
+	// Validate HTML response
+	if resp.StatusCode > 299 {
+		errorMessage := fmt.Errorf("error - Non 200 status code %v: %s", resp.StatusCode, urlString)
+		return "", errorMessage
+	}
+	// Extract HTML body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		errorMessage := fmt.Errorf("error - %v", err)
+		return "", errorMessage
 	}
-	return string(body)
+	return string(body), err
 }
--- main.go
+++ main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"dish_dash_go/extract"
 	"dish_dash_go/transform"
+	"fmt"
 	"sync"
 )
+
+/*
+ I need to figure out how to deal with and recover from panics. For now I
+ need to write a helper funtion that sanitizes the input to avoid panics
+ where possible.
+*/
 
 func main() {
 	data := map[string]interface{}{
@@ -19,6 +26,8 @@ func main() {
 	var waitgroup sync.WaitGroup
 	// RSS will feed this. May connect Amandor's code later.
 	urls := []string{
+		"ttps://icecreamfromscratch.com/cornbread-ice-creams/",
+		"https://icecreamfromscratch.com/cornbread-ice-creams/",
 		"https://icecreamfromscratch.com/cornbread-ice-cream/",
 		"https://icecreamfromscratch.com/lime-ice-cream/",
 		"https://icecreamfromscratch.com/honey-lavender-ice-cream/",
@@ -29,7 +38,12 @@ func main() {
 	for _, url := range urls {
 		go func(u string) {
 			defer waitgroup.Done()
-			html := extract.PageHTML(u)
+			html, err := extract.PageHTML(u)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			// jsonString := transform.ParseHTML(html, data)
 			transform.ParseHTML(html, data)
 		}(url)
 	}
